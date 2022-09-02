de5e978bc1113af012141bf0e94084583c5b104c
kashkarover <ravilek.kashkarov@yandex.ru>
2022-09-02 13:13:55 +0300

    slick-slider

---
+++ 1. trevland/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".sliders").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow:
+      "<button type='button' class='btn-slick-prev btn btn-slider'><img src='images/arrow-left.svg' alt='prev'></button>",
+    nextArrow:
+      "<button type='button' class='btn-slick-next btn btn-slider'><img src='images/arrow-right.svg' alt='next'></button>",
+  });
+});
