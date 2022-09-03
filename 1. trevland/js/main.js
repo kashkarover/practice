6fab2175a96139d5259c800cf99e6ededeedb4cd
kashkarover <ravilek.kashkarov@yandex.ru>
2022-09-03 15:18:17 +0300

    adaptive

--- 1. trevland/js/main.js
+++ 1. trevland/js/main.js
@@ -9,4 +9,10 @@ $(document).ready(function () {
     nextArrow:
       "<button type='button' class='btn-slick-next btn btn-slider'><img src='images/arrow-right.svg' alt='next'></button>",
   });
+
+  $(".mobile-menu").click(function () {
+    $(".mobile-menu span").toggleClass("active");
+    $(".menu").toggleClass("active");
+    $("body").toggleClass("overlay");
+  });
 });
