090d3e06468bda26a6636f3d1b1661029efb06d5
kashkarover <ravilek.kashkarov@yandex.ru>
2022-09-05 10:50:20 +0300

    adaptive end

--- 1. trevland/js/main.js
+++ 1. trevland/js/main.js
@@ -8,6 +8,50 @@ $(document).ready(function () {
       "<button type='button' class='btn-slick-prev btn btn-slider'><img src='images/arrow-left.svg' alt='prev'></button>",
     nextArrow:
       "<button type='button' class='btn-slick-next btn btn-slider'><img src='images/arrow-right.svg' alt='next'></button>",
+    responsive: [
+      {
+        breakpoint: 1401,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1101,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 851,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        },
+      },
+      {
+        breakpoint: 551,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        },
+      },
+    ],
   });
 
   $(".mobile-menu").click(function () {
