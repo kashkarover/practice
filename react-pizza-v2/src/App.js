0021d2fdaa630649b37d4510a6bfe5460a41d07e
kashkarover <ravilek.kashkarov@yandex.ru>
2023-05-09 11:50:23 +0500

    components, scss

--- react-pizza-v2/src/App.js
+++ react-pizza-v2/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div class='wrapper'>
+      <Header />
+      <div class='content'>
+        <div class='container'>
+          <div class='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class='content__title'>Все пиццы</h2>
+          <div class='content__items'>
+            <PizzaBlock title='Мексиканская' price={500} />
+            <PizzaBlock title='Гавайская' price={395} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
