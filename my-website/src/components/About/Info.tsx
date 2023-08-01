f5027e0e3330259776cded213133f939fc57839a
kashkarover <ravilek.kashkarov@yandex.ru>
2023-08-01 16:32:21 +0500

    re

--- my-website/src/components/About/Info.tsx
+++ my-website/src/components/About/Info.tsx
@@ -1,7 +1,12 @@
 import './About.scss';
 
 function Info() {
-  return <div className='info'>sda</div>;
+  return (
+    <div className='info'>
+      <h1>Ravil Kashkarov</h1>
+      <p id='status'>Status</p>
+    </div>
+  );
 }
 
 export default Info;
