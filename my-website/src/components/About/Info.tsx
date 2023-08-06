5e7f208dc06508a81b96918a8662fe403d7c6ee0
kashkarover <ravilek.kashkarov@yandex.ru>
2023-08-06 12:22:24 +0500

    s

--- my-website/src/components/About/Info.tsx
+++ my-website/src/components/About/Info.tsx
@@ -1,29 +1,39 @@
 import './About.scss';
+import InfoBlock from './InfoBlock';
 
 function Info() {
-  const infoCategories = ['Личная информация', 'Образование', 'Контакты'];
+  const infoCategories = [
+    { title: 'Личная информация', age: 26, city: 'Уфа' },
+    {
+      title: 'Образование',
+      univer: 'КНИТУ-КАИ',
+      univer_spec: 'Наноинженерия',
+      univer_degree: 'Бакалавр',
+    },
+    { title: 'Контакты' },
+  ];
 
-  let userInfo: {
-    age: number;
-    city: string;
-    univer: string;
-    univer_spec: string;
-  } = {
-    age: 26,
-    city: 'Уфа',
-    univer: 'КНИТУ-КАИ',
-    univer_spec: 'Наноинженерия',
-  };
+  // let userInfo: {
+  //   age: number;
+  //   city: string;
+  //   univer: string;
+  //   univer_spec: string;
+  // } = {
+  //   age: 26,
+  //   city: 'Уфа',
+  //   univer: 'КНИТУ-КАИ',
+  //   univer_spec: 'Наноинженерия',
+  // };
 
   return (
     <div className='info'>
       <h1>Ravil Kashkarov</h1>
       <p id='status'>Status</p>
       <hr className='hr-line' />
-      {infoCategories.map((value: string, i: number) => {
+      {infoCategories.map((value) => {
         return (
           <div className='info-title'>
-            <h2 key='value'>{value}</h2>
+            <h2 key='value'>{value.title}</h2>
             <hr className='hr-line' />
           </div>
         );
---
+++ my-website/src/components/About/InfoBlock.tsx
@@ -0,0 +1,7 @@
+import './About.scss';
+
+function InfoBlock() {
+  return <div>1</div>;
+}
+
+export default InfoBlock;
